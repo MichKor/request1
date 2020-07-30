1c3d17afc92248863ae05b57faf717c8ab1f9ed0
Michał Korwel <michal.korwel@ttpsc.pl>
2020-07-30 13:57:21 +0200

    Initial commit.

---
+++ src/pl/sda/LinguApp.java
@@ -0,0 +1,8 @@
+package pl.sda;
+
+class LinguApp {
+    public static void main(String[] args) {
+        LinguController controller = new LinguController();
+        controller.mainLoop();
+    }
+}
